a28ecf6706f77366976a26ff55951c4a5be428f2
qushuoqi <qushuoqi@gmail.com>
2025-03-29 09:32:36 +0800

    update java version

--- base-boot-show/src/main/java/com/tiaohe/show/BaseApplication.java
+++ base-boot-show/src/main/java/com/tiaohe/show/BaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tiaohe")
 public class BaseApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(BaseApplication.class, args);
